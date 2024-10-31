a6a26642b0648f9e6751b6345d16881b14789f9c
User <thiwandika.whs02@gmail.com>
2024-10-31 14:52:56 +0530

    create signin and signup pages

---
+++ controller/LoginPageController.js
@@ -0,0 +1,25 @@
+const users = [
+    { email: "sawani.wh@gmail.com", password: "1" },
+    { email: "thiwandika.whs@gmail.com", password: "123" }
+];
+
+function checkCredentials(email, password) {
+    return users.some(user => user.email === email && user.password === password);
+}
+
+function handleLogin(event) {
+    event.preventDefault();
+    const emailInput = document.querySelector('#emailLogin');
+    const passwordInput = document.querySelector('#passwordLogin');
+    const email = emailInput.value;
+    const password = passwordInput.value;
+
+    if (checkCredentials(email, password)) {
+        alert('Login successful!');
+        window.location.href = 'pages.html';
+    } else {
+        alert('Invalid email or password. Please try again.');
+    }
+}
+
+document.querySelector('#loginBtn').addEventListener('click', handleLogin);
